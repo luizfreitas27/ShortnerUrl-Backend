7d6809f8e8f874f5f283be5408d2806234f4e7ca
luizfreitas27 <luizfreitas2712@gmail.com>
2025-12-24 14:48:53 -0400

    fix migrations and compose

--- ShortnerUrl.Api/Program.cs
+++ ShortnerUrl.Api/Program.cs
@@ -1,7 +1,9 @@
 using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using ShortnerUrl.Api.Configurations;
 using ShortnerUrl.Api.Middleware;
+using ShortnerUrl.Api.Persistence;
 using ShortnerUrl.Api.Services.Seed;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -59,6 +61,12 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ShortnerUrlContext>();
+    db.Database.Migrate();
+}
 
 using (var scope = app.Services.CreateScope())
 {
